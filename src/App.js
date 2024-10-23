723b0ca19f95b5bd8730ea9dc74525f2e6f4cdaa
Hass <hassmroueh@gmail.com>
2024-10-23 18:36:57 -0400

    ttd events codealong complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { useState } from "react";
+
 function App() {
+  const[isChecked, setIsChecked] = useState(false)
   return (
-    <div></div>
+    <div>
+      <h1>Selected Pizza Topping</h1>
+      <input 
+      type="checkbox"
+      id="pepperoni"
+      checked={isChecked}
+      aria-checked={isChecked}
+      onChange={(e)=> setIsChecked(e.target.checked)}
+    />
+    <label htmlFor="pepperoni">Add pepperoni</label>
+    <h2>Your Toppings:</h2>
+    <ul>
+      <li>Cheese</li>
+      {isChecked ? <li>Pepperoni</li> : null}
+    </ul>
+    </div>
   )
 }
 
